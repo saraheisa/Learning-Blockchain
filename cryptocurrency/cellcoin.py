bc8f07ebfb4f9088e5dd7a5673efc726b37a4b69
Sara Eisa <prosarahgamal@outlook.com>
2019-10-16 22:32:15 +0200

    add replace_chain functionality

--- cryptocurrency/cellcoin.py
+++ cryptocurrency/cellcoin.py
@@ -81,3 +81,25 @@ class CellCoin:
         self.nodes.add(parsed_url.netloc)
         return parsed_url
     
+    def replace_chain(self):
+        longest_chain = self.get_longest_chain()
+        if longest_chain:
+            self.chain = longest_chain
+            return True
+        return False
+        
+    def get_longest_chain(self):
+        network = self.nodes
+        longest_chain = None
+        max_length = len(self.chain)
+        for node in network:
+            response = requests.get(f'http://{node}/get_chain')
+            if response.status_code == 200:
+                length = response.json()['length']
+                chain = response.json()['chain']
+                if length > max_length and self.is_chain_valid(chain):
+                    max_length = length
+                    longest_chain = chain
+                    
+        return longest_chain
+        
