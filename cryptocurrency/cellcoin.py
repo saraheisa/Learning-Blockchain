4c514c757ca7c1ff5b444312edfa5a20ee43d5c0
Sara Eisa <prosarahgamal@outlook.com>
2019-10-16 22:12:28 +0200

    set up the cellcoin file

---
+++ cryptocurrency/cellcoin.py
@@ -0,0 +1,64 @@
+import datetime  # used to store the creation date of each block
+import hashlib
+import json
+import requests
+
+from urllib.parse import urlparse
+
+# building a blockchain
+class CellCoin:
+    
+    def __init__ (self):
+        self.chain = []
+        self.create_block(proof=1, prev_hash='0') # create the genesis block
+        
+    def create_block(self, proof, prev_hash):
+        block = {'index': len(self.chain)+1,
+                 'timestamp': str(datetime.datetime.now()),
+                 'proof': proof,
+                 'prev_hash': prev_hash}
+        self.chain.append(block)
+        return block
+    
+    def get_prev_block(self):
+        return self.chain[-1]
+    
+    def proof_of_work(self, prev_proof):
+        new_proof = 1
+        check_proof = False
+        while check_proof is False:
+            hash_operation = hashlib.sha256(str(new_proof**2 - prev_proof**2).encode()).hexdigest()
+            if hash_operation[:4] == '0000':
+                check_proof = True
+            else:
+                new_proof += 1
+        return new_proof
+    
+    def hash(self, block):
+        # convert dict to str and encode it
+        # didn't use str() because the block will be stored as json later
+        encoded_block = json.dumps(block, sort_keys=True).encode() 
+        return hashlib.sha256(encoded_block).hexdigest()
+    
+    def is_chain_valid(self, chain):
+        prev_block = chain[0]
+        block_index = 1
+        
+        while block_index < len(chain):
+            block = chain[block_index]
+            
+            # 1st check: checks if the previous hash is correct
+            if block['prev_hash'] != self.hash(prev_block):
+                return False
+            
+            # 2nd check: if the hash starts with 4 zeros
+            prev_proof = prev_block['proof']
+            proof = block['proof']
+            hash_operation = hashlib.sha256(str(proof**2 - prev_proof**2).encode()).hexdigest()
+            if hash_operation[:4] != '0000':
+                return False
+            
+            prev_block = block
+            block_index += 1
+            
+        return True
